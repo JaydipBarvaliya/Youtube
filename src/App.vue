1d38973c9bb7a1de94e1ae5c721a27cb8fb5d8ef
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2025-07-03 17:48:00 -0400

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="container">
+        
+        <SearchBar v-on:termChange="onTermChange"></SearchBar>
+        <div class="row">
+
+          <VideoDetail v-bind:toDetail="selectedvideo"></VideoDetail>
+        <VideoList v-bind:videos="videos" @mainparent="MainParent"></VideoList>
+        
+        </div>
+        
+
+
+
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
+import VideoDetail from "./components/VideoDetail";
+const API_KEY = "YOUR_YOUTUBE_API_KEY";
+export default {
+  name: "App",
+
+  data: function() {
+    return { videos: [], selectedvideo: null};
+},
+  components: {
+    SearchBar,
+    VideoList,
+    VideoDetail
+  },
+
+  methods: {
+    onTermChange: function(searchTerm) {
+      console.log(searchTerm);
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: API_KEY,
+            type: "video",
+            part: "snippet",
+            q: searchTerm
+          }
+        })
+        .then(response => {
+          this.videos = response.data.items;
+        });
+    },
+    MainParent: function(video_finale) {
+     this.selectedvideo=video_finale;
+    }
+  }
+};
+</script>
+
